9798d8fd4c201d58acc567f706f760628baf4c9c
Таня <tanya.savenkova2005@gmail.com>
2025-10-27 15:08:35 +0300

    Создание схем для микросервиса с заказами

---
+++ service_orders/schemas.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+from typing import List
+
+class OrderCreate(BaseModel):
+    user_id: str
+    items: List[dict]
+    total: float
+
+class OrderUpdate(BaseModel):
+    status: str
